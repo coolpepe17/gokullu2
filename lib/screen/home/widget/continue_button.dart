08e705c02810af008008df34e4b35e40ce325e18
coolpepe17 <coolpepe@gmail.com>
2021-02-05 12:43:43 +0530

    Mobile_register

--- lib/screen/home/widget/continue_button.dart
+++ lib/screen/home/widget/continue_button.dart
@@ -26,7 +26,7 @@ class ContinueButton extends StatelessWidget {
                   context,
                   MaterialPageRoute(
                     builder: (context) {
-                      return RegisterScreen();
+                      return LoginScreen();
                     },
                   ),
                 );
